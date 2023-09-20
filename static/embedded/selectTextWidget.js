1c3aa4246219df5c3fe4a00b5b7bfeb25a8d723a
Vamsi <vamsi@surfly.com>
2023-09-20 11:06:59 +0200

    feat: added widget which can select text and show a button

---
+++ static/embedded/selectTextWidget.js
@@ -0,0 +1,100 @@
+// Create a <style> element and set its content to your CSS styles
+const styleElement = document.createElement("style");
+styleElement.textContent = `
+  .hidden {
+    display: none;
+  }
+
+  #popup-button {
+    position: relative;
+    background: linear-gradient(45deg, #3a0764, #690bb6);
+    color: #fff;
+    padding: 10px 10px;
+    border: none;
+    border-radius: 20px;
+    cursor: pointer;
+    width: 40px;
+    box-shadow: #64646f33 0px 7px 29px 0px;
+  }
+
+  #popup-section {
+    padding: 10px;
+    border-radius: 30px;
+    max-width: 300px;
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    position: absolute;
+  }
+
+  .lookup-content {
+    width: 400px;
+    height: 150px;
+    border-radius: 8px;
+    padding: 10px;
+    background: linear-gradient(45deg, #3a07646b, #690bb651);
+    color: white;
+  }
+
+  #popup-section.hidden {
+    display: none;
+  }
+`;
+
+// Append the <style> element to the document's <head>
+document.head.appendChild(styleElement);
+
+// Create the HTML template as a string
+const popupTemplate = `
+  <div id="popup-section" class="hidden">
+    <button id="popup-button">
+      <i class="fa-solid fa-magnifying-glass"></i>
+    </button>
+    <div class="lookup-content hidden" id="lookup-content">
+      Lookup content here
+    </div>
+  </div>
+`;
+
+// Convert the HTML template string into DOM elements
+const parser = new DOMParser();
+const popupElements = parser.parseFromString(popupTemplate, "text/html").body
+  .firstChild;
+
+// Append the popupElements to the body or another suitable container
+document.body.appendChild(popupElements);
+
+const popupButton = document.getElementById("popup-section");
+const lookupContent = document.getElementById("lookup-content");
+// Function to show the popup button next to selected text
+function showPopupButton() {
+  const selectedText = window.getSelection().toString().trim();
+  if (selectedText !== "") {
+    // Get the selection range
+    const selectionRange = window.getSelection().getRangeAt(0);
+
+    // Get the position of the selected text
+    const rect = selectionRange.getBoundingClientRect();
+
+    // Set the position of the popup button
+    popupButton.style.top = rect.bottom + window.scrollY + "px";
+    popupButton.style.left = rect.right + window.scrollX + "px";
+
+    // Show the popup button
+    popupButton.classList.remove("hidden");
+  } else {
+    // Hide the popup button if no text is selected
+    popupButton.classList.add("hidden");
+    lookupContent.classList.add("hidden");
+  }
+}
+
+// Event listener for text selection
+document.addEventListener("mouseup", showPopupButton);
+
+// Update the position of the popup when the window is resized
+window.addEventListener("resize", showPopupButton);
+
+popupButton.addEventListener("click", function () {
+  lookupContent.classList.remove("hidden");
+});

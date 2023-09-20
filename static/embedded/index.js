853e323626dbceca170bd15bf0ed8f181d8bb081
Mirhossein Mousavi <mir@surfly.com>
2023-09-20 10:08:01 +0200

    Comment askAI example

--- static/embedded/index.js
+++ static/embedded/index.js
@@ -5,8 +5,8 @@ console.log('\nEmbedded script is alive!\n\n');
     const { askAI } = await import('./ai.js');
 
     const input = "Despite a police complaint being registered in May, no investigation into the incident took place until a video of it surfaced on social media in July. That's when the conflict in Manipur caught the attention of many in India and around the world.";
-    const answer = await askAI('summarise', input);
-    console.log(answer);
+    // const answer = await askAI('summarise', input);
+    // console.log(answer);
 
     submitLog('test');
 

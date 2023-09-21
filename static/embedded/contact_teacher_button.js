f63ed62f8b99aaa0301a4ac43f25d489f178612b
Mirhossein Mousavi <mir@surfly.com>
2023-09-21 16:16:08 +0200

    Add teacher joins popup

--- static/embedded/contact_teacher_button.js
+++ static/embedded/contact_teacher_button.js
@@ -13,24 +13,9 @@ button.style.padding = "20px";
 
 
 button.addEventListener("click", async function() {
-    const apiKey = "5f774775d45b4328b2c8976a841418c8";
-    const url = "https://wikipedia.org";
+    const popup = document.getElementById("teacher-joins-popup");
+    popup.classList.remove("hidden");
 
-    const params = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            url,
-            queued: true,
-        }),
-    };
-
-    const res = await fetch(`https://surfly.online/v2/sessions/?api_key=${apiKey}`, params);
-    const data = await res.json();
-    sessionId = data.session_id;
-    window.location.href = data.leader_link;
 });
 
 // Add the button to the page
